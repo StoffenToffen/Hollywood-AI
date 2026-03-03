ab7fc1d47c3f872262ed2a37ff1afac58e37a150
Steffen Tveit <steffentveit@outlook.com>
2026-03-03 16:19:28 +0100

    chore: Refactored CSS for side navigation and search components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -25,7 +25,7 @@ const fetchTopMovies = async () => {
 
 const Page = async () => {
   const selectedMovies = await fetchSelectedMovies();
-  const topMovies = await fetchTopMovies()
+  const topMovies = await fetchTopMovies();
 
   return (
     <div className="page-wrapper">
