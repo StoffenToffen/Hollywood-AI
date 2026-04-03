07166d2058fbd05e5087ae296f8872756817c4ac
Steffen Tveit <steffentveit@outlook.com>
2026-04-03 12:35:33 +0200

    feat: Added structure to favourites page for when there are no favourites

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,7 +1,32 @@
+import Nav from "@/components/dashboard/Nav";
+import Search from "@/components/dashboard/Search";
+
+import "./page.css"
+
 const Page = () => {
   return (
-    <div>
-      
+    <div className="page-wrapper">
+      <Nav />
+
+      <div className="page-content">
+        <Search />
+
+        <div className="page-row">
+          <header className="header">
+            <h1 className="favourites__title">Saved Movies</h1>
+            <span className="favourites__subtext">0 Movies</span>
+          </header>
+        </div>
+
+        <section className="favourites">
+          <div className="page-row">
+            <div className="favourites__empty">
+              <strong>Save your favourite movies!</strong>
+              <span>When you save a movie, it will appear here.</span>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
