a28f14f9afa129166a209f1d8f26c40ba3c0de4a
Steffen Tveit <steffentveit@outlook.com>
2026-04-14 15:07:37 +0200

    feat: Added skeleton loading state to favourites page

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -13,18 +13,20 @@ import { useUserStore } from "@/zustand/userStore";
 import { fetchData } from "../fetches";
 
 import "./page.css";
-import "../dashboard/page.css";
+import FavouritesSkeleton from "@/components/loading-states/FavouritesSkeleton";
 
 const Page = () => {
-  const [favourites, setFavourites] = useState<string[]>([]);
-  const [favouriteMovies, setFavouriteMovies] = useState<Movie[]>([]);
+  const [favouriteMovies, setFavouriteMovies] = useState<Movie[]>();
 
   const uid = useUserStore((state) => state.uid);
+  const userFetched = useUserStore((state) => state.userFetched);
   const toggleLoginModal = useModalStore((state) => state.toggleLoginModal);
 
   useEffect(() => {
+    if (!userFetched) return;
+
     if (!uid) {
-      setFavourites([]);
+      setFavouriteMovies([]);
       return;
     }
 
@@ -34,30 +36,21 @@ const Page = () => {
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-          setFavourites(docSnap.data().favourites);
+          const promises = docSnap
+            .data()
+            .favourites.map((favourite: string) =>
+              fetchData<Movie>("Movies", favourite),
+            );
+
+          const fetchedData = await Promise.all(promises);
+
+          setFavouriteMovies(fetchedData);
         }
       } catch (err) {
         console.error(err);
       }
     })();
-  }, [uid]);
-
-  useEffect(() => {
-    if (favourites.length < 1) {
-      setFavouriteMovies([]);
-      return;
-    }
-
-    (async () => {
-      const promises = favourites.map((favourite) =>
-        fetchData<Movie>("Movies", favourite),
-      );
-
-      const fetchedData = await Promise.all(promises);
-
-      setFavouriteMovies(fetchedData);
-    })();
-  }, [favourites]);
+  }, [userFetched, uid]);
 
   return (
     <div className="page-wrapper">
@@ -66,54 +59,60 @@ const Page = () => {
       <div className="page-content">
         <Search />
 
-        <div className="page-row">
-          <header className="header">
-            <h1 className="favourites__title">Saved Movies</h1>
-            <span className="favourites__subtext">
-              {favouriteMovies.length} Movies
-            </span>
-          </header>
-        </div>
-
-        <section className="favourites">
-          <div className="page-row">
-            {!uid ? (
-              <div className="favourites__row">
-                <Image
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  src="/assets/login.webp"
-                  alt="person in front of a computer"
-                  className="favourites__img"
-                />
-
-                <span className="favourites__text">
-                  Sign in to see your favourited movies
+        {!favouriteMovies ? (
+          <FavouritesSkeleton />
+        ) : (
+          <>
+            <div className="page-row">
+              <header className="header">
+                <h1 className="favourites__title">Saved Movies</h1>
+                <span className="favourites__subtext">
+                  {favouriteMovies.length || 0} Movies
                 </span>
+              </header>
+            </div>
 
-                <button
-                  type="button"
-                  className="favourites__btn"
-                  onClick={toggleLoginModal}
-                >
-                  Login
-                </button>
+            <section className="favourites">
+              <div className="page-row">
+                {!uid ? (
+                  <div className="favourites__row">
+                    <Image
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      src="/assets/login.webp"
+                      alt="person in front of a computer"
+                      className="favourites__img"
+                    />
+
+                    <span className="favourites__text">
+                      Sign in to see your favourited movies
+                    </span>
+
+                    <button
+                      type="button"
+                      className="favourites__btn"
+                      onClick={toggleLoginModal}
+                    >
+                      Login
+                    </button>
+                  </div>
+                ) : favouriteMovies.length > 0 ? (
+                  <div className="favourites__movies">
+                    {favouriteMovies.map((movie) => (
+                      <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                  </div>
+                ) : (
+                  <div className="favourites__empty">
+                    <strong>Save your favourite movies!</strong>
+                    <span>When you save a movie, it will appear here.</span>
+                  </div>
+                )}
               </div>
-            ) : favouriteMovies.length > 0 ? (
-              <div className="favourites__movies">
-                {favouriteMovies.map((movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
-                ))}
-              </div>
-            ) : (
-              <div className="favourites__empty">
-                <strong>Save your favourite movies!</strong>
-                <span>When you save a movie, it will appear here.</span>
-              </div>
-            )}
-          </div>
-        </section>
+            </section>
+          </>
+        )}
       </div>
     </div>
   );
